7d6740ed5bc9ee693c1db7fec57d33465aeecccf
Usman Rashid Khan <khan962277@gmail.com>
2020-10-06 14:43:02 +0530

    intial commit

---
+++ src/JS/index.js
@@ -0,0 +1,16 @@
+var lastScrollTop= 0;
+navbar=document.getElementById("header");
+window.addEventListener('scroll',function(){
+    var scrollTop = window.pageYOffset || document
+    .documentElement.scrollTop;
+    if(scrollTop>lastScrollTop){
+        navbar.style.top="-90px";
+    } else{
+        navbar.style.top="0";
+
+
+    }
+    lastScrollTop=scrollTop;
+})
+
+
